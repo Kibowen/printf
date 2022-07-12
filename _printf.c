fe76c1143ed66494ebc74e66906a5e0bb9846ff8
Trevor <chelimotrevorkibowen@gmail.com>
2022-07-12 23:20:43 +0300

    removed main file and modifies printf

--- _printf.c
+++ _printf.c
@@ -42,27 +42,6 @@ int _printf(const char *format, ...)
 					_putchar('%');
 					total += 1;
 					break;
-				case 'd':
-					total += _print_int((long)(va_arg(args, int)));
-					break;
-				case 'i':
-					total += _print_int((long)(va_arg(args, int)));
-					break;
-				case 'b':
-					total += to_Binary(va_arg(args, int));
-					break;
-				case 'u':
-					total += _print_int(va_arg(args, unsigned int));
-					break;
-				case 'o':
-					total += to_Octal(va_arg(args, int));
-					break;
-				case 'x':
-					total += to_Hexa(va_arg(args, int));
-					break;
-				case 'X':
-					total += to_Hexa(va_arg(args, int));
-					break;
 				default:
 					_putchar('%');
 					_putchar(format[count]);
